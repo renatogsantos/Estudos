10097c42c43a7ee60b90cf95c176bf1eaa987c00
Renato Gomes Dos Santos <renato.work.art@gmail.com>
2020-01-07 08:51:52 -0300

    Testando
    teste com mais uma var

--- exercicio1.js
+++ exercicio1.js
@@ -1,11 +1,12 @@
 var end = {
+  nome: "Renato G.",
   rua: "tamandaré",
   numero: 91,
   bairro: "liberdade",
   cidade: "São Paulo",
   uf: "SP"
-}
+};
 function info(end){
-  return 'O nosso amigo mora em ' + end.cidade + ' / ' + end.uf + ', no bairro' + end.bairro + ', na rua ' + end.rua + ' com o número ' + end.numero
-}
-console.log(info(end))
+  return 'O cliente '+ end.nome + ' mora em ' + end.cidade + ' / ' + end.uf + ', no bairro' + end.bairro + ', na rua ' + end.rua + ' com o número ' + end.numero;
+};
+console.log(info(end));
