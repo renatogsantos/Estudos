6631987d9336c993c7e5dfcc80e019894feaa6d5
Renato Gomes Dos Santos <renato.work.art@gmail.com>
2020-01-09 17:30:41 -0300

    teste
    teste

---
+++ webmoderno/exercicios.js
@@ -0,0 +1,13 @@
+var func = {
+  name: "renato",
+  idade: 26,
+  peso: 76.8,
+}
+
+function teste(){
+  return 'Olá ' + func.name + ', seu peso é ' + func.peso + ' e sua idade é ' + func.idade
+}
+
+console.log(teste());
+
+//Olá renato, seu peso é 76.8 e sua idade é 26
